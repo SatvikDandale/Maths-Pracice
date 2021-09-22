5c2af88db2bbb6a238de8a7a9f22898b2b20d2d1
Satvik Dandale <satvik.dandale@gmail.com>
2021-09-22 21:16:14 +0530

    Site Changes

---
+++ precache-manifest.038fea779aaa61b6b31953a00eb96dac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11692a24a2d48274a3a8a5b77df323c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a32bff0799068ae95226",
+    "url": "/static/css/2.b204753f.chunk.css"
+  },
+  {
+    "revision": "7da1f2a34701ab710a91",
+    "url": "/static/css/main.27268601.chunk.css"
+  },
+  {
+    "revision": "a32bff0799068ae95226",
+    "url": "/static/js/2.2fc18b91.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.2fc18b91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7da1f2a34701ab710a91",
+    "url": "/static/js/main.7afbe8f9.chunk.js"
+  },
+  {
+    "revision": "4e520415db9accf52cad",
+    "url": "/static/js/runtime-main.d3b29b45.js"
+  }
+]);
